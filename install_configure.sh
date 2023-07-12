5be3b08c87dc3bea45eb3159f469773be2cd63ab
thomasldg7 <66885811+thomasldg7@users.noreply.github.com>
2023-07-12 10:56:51 +0200

    Update install_configure.sh

--- install_configure.sh
+++ install_configure.sh
@@ -1,4 +1,5 @@
 apt-get update && apt-get install nginx -y
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+wget https://raw.githubusercontent.com/thomasldg7/nginxLB/main/nginx.conf
 mv nginx.conf /etc/nginx/nginx.conf
 systemctl restart nginx
